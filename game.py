5a976add88dd945bc3e1ed898437af80921aa907
KauliFlowar <66041781+KauliFlowar@users.noreply.github.com>
2020-05-28 09:39:53 -0400

    Update game.py
    quality of life changes

--- game.py
+++ game.py
@@ -8,19 +8,33 @@ import random
 
 screen_width = 100
 
+intro_story = ["Once upon a time, there was a world named Isuren, which was home to many amazing creatures.\n",
+               "Isuren was divided into various kingdoms, each with their own element.\n",
+               "Each kingdom's power rests in an elemental crystal, sealed within the kingdom.\n",
+               "One day, an elemental crystal was stolen and the kingdom's power slowly faded away.\n",
+               "Within a few years, multiple different elemental crystals were stolen and their kingdoms fell.\n",
+               "Soon enough, every single kingdom had lost their elemental crystal.\n",
+               "Then, a prophecy has been spoken about a young hero, who can find and return the crystals from the thief.\n",
+               "Isuren will be saved one day...\n"]
+
+tutorial_story = ["I open my eyes.\n",
+                  "I feel as if they have been closed for eternity.\n",
+                  "Where am I...?\n"]
+
+
 # Game Execution
 def title_screen_menu():
     # Allows the player to select menu options, which is case-sensitive.
     option = input("> ")
     if option.lower() == "play":
-        setup_game()
+        setup_game(intro_story, 0.045, 1, 1)
     elif option.lower() == "quit":
         sys.exit()
     while option.lower() not in ['play', 'quit']:
         print("Invalid command, please try again")
         option = input("> ")
         if option.lower() == "play":
-            setup_game()
+            setup_game(intro_story, 0.045, 1, 1)
         elif option.lower() == "quit":
             sys.exit()
 
@@ -30,32 +44,30 @@ def title_screen():
     print("█ Mini Fantasy █")
     print("█   Text Game  █")
     print("█" * 16)
-    print("     .:Play:.   ")
-    print("     .:Quit:.   ")
+    print("    .:Play:.   ")
+    print("    .:Quit:.   ")
     title_screen_menu()
 
 
-def setup_game():
+def setup_game(story, text_speed, wait_time, output):
     print("\n")
-    story = ["Once upon a time, there was a world named Isuren, which was home to many amazing creatures.\n",
-             "Isuren was divided into various kingdoms, each with their own element.\n",
-             "Each kingdom's power rests in an elemental crystal, sealed within the kingdom.\n",
-             "One day, an elemental crystal was stolen and the kingdom's power slowly faded away.\n",
-             "Within a few years, multiple different elemental crystals were stolen and their kingdoms fell.\n",
-             "Soon enough, every single kingdom had lost their elemental crystal.\n",
-             "Then, a prophecy has been spoken about a young hero, who can find and return the crystals from the thief.\n",
-             "Isuren will be saved one day...\n"]
     story_num = int(len(story))
     story_current = 0
-    text_speed = 0.045
     while story_current < story_num:
         for char in story[story_current]:
             sys.stdout.write(char)
             sys.stdout.flush()
             time.sleep(text_speed)
-        time.sleep(1)
+        time.sleep(wait_time)
         story_current = story_current + 1
-    time.sleep(2)
+    print("\n")
     # he said it couldn't be done
+    if output == 1:
+        print("█" * 25)
+        print("█ The adventure begins! █")
+        print("█" * 25)
+        time.sleep(2)
+        setup_game(tutorial_story, 0.06, 1.5, 2)
+
 
 title_screen()
