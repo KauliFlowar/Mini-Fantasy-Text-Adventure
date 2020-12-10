ead9df534278b27a1163349b8a95cb8947bf4db4
KauliFlowar <66041781+KauliFlowar@users.noreply.github.com>
2020-12-09 19:50:55 -0500

    finished the thing, just gotta implement it
    setup_game() is gonna need a lot of work...

--- storytesting.py
+++ storytesting.py
@@ -1,15 +1,15 @@
 # import os
 
-story = open("storytest.txt", "r")
+story = open("gamestory.txt", "r")
 indexing_char = "~"
 filtering_count = "lol ok"
 currently_accessing = 0
-current_sub_accessing = 0
 
 dialogue = {}
+partition = ""
 
 while filtering_count != " ":
-    filtering_count = story.readline().replace("/n", "").strip()
+    filtering_count = story.readline().replace("\n", "").strip()
     if indexing_char in filtering_count and filtering_count.find(indexing_char) == 0:
         partition = filtering_count.replace(indexing_char, "").strip()
         print(partition)
@@ -17,18 +17,25 @@ while filtering_count != " ":
             filtering_count = " "
             break
         else:
-            currently_accessing = filtering_count[1] + filtering_count[2] + filtering_count[3] + filtering_count[4]
+            try:
+                currently_accessing = filtering_count[1] + filtering_count[2] + filtering_count[3] + filtering_count[4]
+            except IndexError:
+                currently_accessing = filtering_count[1] + filtering_count[2]
+            finally:
+                dialogue[partition] = ""
     else:
-        if currently_accessing in dialogue:
-            dialogue[currently_accessing] = dialogue[currently_accessing] + "\n" + filtering_count
+        if dialogue[partition] == "":
+           dialogue[partition] = filtering_count
         else:
-            dialogue[currently_accessing] = filtering_count
-            print(dialogue[currently_accessing])
+            dialogue[partition] = dialogue[partition] + "\n" + filtering_count
 
-for x in dialogue:
-    print(dialogue[x])
-print(dialogue)
-print(dialogue["b2p1"])
+
+# for x in dialogue:
+#     print(dialogue[x])
+# print(dialogue)
+print(dialogue["t4"])
+
+
 # lol[1][1] = "lol"
 # print(lol)
 # for player_name use {0} so that you can get the 1st argument in a .Format()
